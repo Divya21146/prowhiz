287ff3f9f809849c84c46d80340a2cbb687f9721
Divya21146 <divi21146@gmail.com>
2024-03-11 23:26:07 +0530

    deleted expired plugin and comleted 3 pages without design

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -2,6 +2,9 @@
 function enqueue_font_awesome() {
     wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' );
     wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', array(), null, true);
+    wp_enqueue_style('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css');
+	wp_enqueue_style('owl-theme-default', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css');
+	wp_enqueue_script('owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', array('jquery'), '', true);
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
 
@@ -131,3 +134,134 @@ function load_more_posts_categories()
     wp_reset_postdata();
     wp_die();
 }
+
+//proconnect contents
+function proconnect() {
+$args = array(
+    'post_type' => 'pro_connect',
+    'post_status' => 'publish',
+);
+$query = new WP_Query($args);
+if ($query->have_posts()) : ?>
+    <?php while ($query->have_posts()) : $query->the_post(); ?>
+        <div class="pro_connect"> 
+            <?php 
+                if (get_field('image')) {
+                    $image = get_field('image');
+                    echo '<img src="' . $image . '" alt="image" />';
+                }   
+            ?>             
+            <div class="cat">
+            <h5><?php the_title(); ?></h5>
+                <div><?php echo get_field('description'); ?></div>
+                <div><?php echo get_field('name'); ?></div>
+            </div>
+            <?php $youtube_link = get_field('youtube_link'); ?>
+            <div><button class="event-btn"><a href="<?php echo esc_url($youtube_link); ?>" target="_blank">View Now</a></button></div>
+        </div>
+    <?php endwhile; ?>
+<?php else : ?>
+    <p>No Pro Connects found</p>
+<?php endif;
+wp_reset_postdata();
+}
+
+function proconnect_shortcode() {
+    ob_start();
+    proconnect();
+    return ob_get_clean();
+}
+add_shortcode('proconnect_shortcode', 'proconnect_shortcode');
+
+//testimonial carousel
+function testimonial_carousel_shortcode() {
+    ob_start();
+    
+    $testimonials = new WP_Query(array(
+        'post_type' => 'testimonial',
+        'posts_per_page' => -1,
+    ));
+
+    if ($testimonials->have_posts()) :
+    ?>
+	<div class="testimonial-carousel-wrapper">
+    	<div class="testimonial-carousel owl-carousel">
+            <?php while ($testimonials->have_posts()) : $testimonials->the_post(); ?>
+                <div class="testimonial-item"> 
+					<div class="testimonial-profile">
+					<div class="testimonial-image"><img src="<?php the_field('photo'); ?>" alt=""></div>
+                    <div class="testimonial-employee">
+                        <p><?php the_field('feedback'); ?></p>
+                        <div class="testimonial-rating">
+                    <?php 
+                    $rating = get_field('ratings');
+                    for ($i = 1; $i <= 5; $i++) {
+                        if ($i <= $rating) {
+                            echo '<i class="fa fa-star" style="color: #f1c40f;"></i>';
+                        } else {
+                            echo '<i class="fa fa-star" style="color: #ccc;"></i>';
+                        }
+                    }
+                    ?>
+                </div>
+                <div class="testimonial-name"><?php the_title(); ?></div>
+						<div class="role-and-company-name"><?php the_field('designation'); ?></div>
+					</div>
+					</div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+		<div class="testimonial-carousel-nav">
+            <button class="testimonial-carousel-prev">prev</button>
+            <button class="testimonial-carousel-next">next</button>
+        </div>
+	</div>	
+		<script>
+        jQuery(document).ready(function($) {
+             // Function to set equal height for card items
+             function setEqualHeight(selector) {
+                var tallestHeight = 0;
+                $(selector).each(function() {
+                    var currentHeight = $(this).height();
+                    if (currentHeight > tallestHeight) {
+                        tallestHeight = currentHeight;
+                    }
+                });
+                $(selector).height(tallestHeight);
+            }
+            
+            // Call the function on window load and resize
+            $(window).on('load resize', function() {
+                setEqualHeight('.testimonial-carousel .testimonial-item');
+            });
+            $('.testimonial-carousel').owlCarousel({
+                items: 1,
+                loop: true,
+                margin: 30,
+                nav: false,
+                dots: false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    768:{
+                        items:1
+                    }
+                }
+            });
+			$('.testimonial-carousel-prev').click(function() {
+                $('.testimonial-carousel').trigger('prev.owl.carousel');
+            });
+            $('.testimonial-carousel-next').click(function() {
+                $('.testimonial-carousel').trigger('next.owl.carousel');
+            });
+        });
+    </script>
+    <?php
+    endif;
+
+    wp_reset_postdata();
+
+    return ob_get_clean();
+}
+add_shortcode('testimonial_carousel', 'testimonial_carousel_shortcode');
